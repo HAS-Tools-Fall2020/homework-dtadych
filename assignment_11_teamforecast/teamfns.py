a5bf3a750ed8427c5814dc478e2d37a9fc5c0f80
DanielleTadych <dtadych@email.arizona.edu>
2020-11-05 09:42:12 -0700

    Team forecast stuff and eval

---
+++ assignment_11_teamforecast/teamfns.py
@@ -0,0 +1,7 @@
+# Team Functions
+# ~~~~~~
+# This is a script we can read put all of our functions and read into
+# the main forecast script
+
+# This is where I will be living for the next couple days
+# -Danielle
